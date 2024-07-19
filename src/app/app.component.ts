d2e1c735f050f9c38fa34039b44e708113c4a09d
Keidson Roby <keidsondesigner@gmail.com>
2024-07-19 14:54:41 -0400

    feat: Minha primeira Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MinhaPrimeiraClass } from './fundamentos/01classes/minhaPrimeiraClass';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fundamentos-oop';
+
+  ngOnInit() {
+    const person1 = new MinhaPrimeiraClass('John', 30, 'Programmer');
+    console.log(person1);
+    console.log(person1.getAge());
+  }
 }
 
 
---
+++ src/app/fundamentos/01classes/minhaPrimeiraClass.ts
@@ -0,0 +1,35 @@
+export class MinhaPrimeiraClass {
+  private name: string;
+  private age: number;
+  private position: string;
+
+  constructor(nome: string, age: number, position: string) {
+    this.name = nome;
+    this.age = age;
+    this.position = position;
+  }
+
+  public getPosition() {
+    return this.position;
+  }
+
+  public getName() {
+    return this.name;
+  }
+
+  public getAge() {
+    return this.age;
+  }
+
+  public setPosition(position: string) {
+    this.position = position;
+  }
+
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  public setAge(age: number) {
+    this.age = age;
+  }
+}
