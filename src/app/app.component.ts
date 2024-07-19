6c99d572ecc649be2b2d0474f996d85aa444b424
Keidson Roby <keidsondesigner@gmail.com>
2024-07-19 08:13:57 -0400

    feat: Conceito de Prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fundamentos-oop';
-
 }
+
+
+
+// CONCEITO DE PROTOTYPE
+
+// Interface são contratos, onde a class é quem vai implementa-lós;
+
+// interface Protopype {
+//   clone(): Protopype;
+// }
+
+// export class Person implements Protopype {
+
+//   constructor(public name: string, public age: number) {}
+
+//   clone(): this {
+//     // Isso não é um clone!
+//     // estamos apenas criando um novo objeto
+//     // que tem este Objeto como protótiopo"prototype"(tem os mesmos valores dos anteriores)
+//     const newPerson = Object.create(this);
+//     return newPerson;
+//   }
+// }
+
+// const person1 = new Person('John', 30);
+// const person2 = person1.clone();
+
+// console.log(person1.name); // John
+// console.log(person2.name); // John
