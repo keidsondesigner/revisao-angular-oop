d5ac34ecee1e738c7caeb34cc76dc11a13ac73c5
Keidson Roby <keidsondesigner@gmail.com>
2024-07-21 07:38:46 -0400

    feat: Criado Class Conta Corrente

--- src/app/projeto-conta-bancaria/modelos/contaBancaria.ts
+++ src/app/projeto-conta-bancaria/modelos/contaBancaria.ts
@@ -2,7 +2,7 @@
 export abstract class ContaBancaria {
   constructor(
     public titular: string,
-    protected saldo: number = 0,
+    protected saldo: number = 0, // consigo acessar o saldo com protected, apenas quando estendo essa class mãe
   ) {}
 
   depositar(deposito: number) {
@@ -11,6 +11,8 @@ export abstract class ContaBancaria {
 
   // método abstrato, quem herdar essa classe, tem que implementar;
   abstract sacar(saque: number): void;
+
+  abstract getTipoConta(): string;
 
   getSaldo(): number {
     return this.saldo;
