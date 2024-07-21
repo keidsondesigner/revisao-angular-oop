2a7746d342739a6d99144ac79736cd5e0123c620
Keidson Roby <keidsondesigner@gmail.com>
2024-07-21 08:10:43 -0400

    feat: Criado Compoent Cadrasto ContaBancaria

---
+++ src/app/projeto-conta-bancaria/conta-bancaria/conta-bancaria.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conta-bancaria',
+  templateUrl: './conta-bancaria.component.html',
+  styleUrls: ['./conta-bancaria.component.css']
+})
+export class ContaBancariaComponent {
+
+}
